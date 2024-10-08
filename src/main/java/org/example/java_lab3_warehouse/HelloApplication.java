dc716842c32c44dfdc07cf2907d085be5a2b87f1
Conny <pedalconny@hotmail.com>
2024-10-08 08:57:10 +0200

    initial commit

---
+++ src/main/java/org/example/java_lab3_warehouse/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.java_lab3_warehouse;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
